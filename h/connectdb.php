0e5fa3e3ef9049d8b4fe122577bb351d6acac19d
jiraphorn6425 <65010914625@msu.ac.th>
2025-02-04 11:54:26 +0700

    1

--- h/connectdb.php
+++ h/connectdb.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pwd = "";
-$db = "shop";
+$pwd = "qq123456";
+$db = "shop4625";
 $conn = mysqli_connect($host, $user, $pwd) or die ("No connect");
 mysqli_select_db($conn, $db) or die ("No select database");
 mysqli_query($conn, "set names utf8");
